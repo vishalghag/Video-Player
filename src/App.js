3060f3e4f43be16859e0c4e2d6158270af04a3f3
Vishal Ghag <vishal.ghag@i2econsulting.com>
2024-02-17 01:25:16 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline bg-green-400">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
